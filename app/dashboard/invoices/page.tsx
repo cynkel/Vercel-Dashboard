876130f0dcd555c8e13c44f47db7d4452f49faa4
Kelvin <kelxir@outlook.com>
2024-10-08 13:35:19 +0000

    Error fix 1

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -15,7 +15,7 @@ export default async function Page({
       page?: string;
     };
   }) {
-  const query = await searchParams?.query || '';
+  const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
 
   const totalPages = await fetchInvoicesPages(query);
